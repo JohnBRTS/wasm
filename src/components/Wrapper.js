08af38229c5a06f27658e7a4a565f4b2d62b3ce7
John Blackwell <john.blackwell@rtslabs.com>
2019-03-22 13:56:46 -0400

    feeling a lot closer

--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,23 +1,179 @@
-import React, { useEffect, useState } from 'react';
+import React, { Component } from 'react';
 import Porcupine from './porcupine';
-
-const Wrapper = () => {
-  // const obj = useWasm({
-  //   url: './porcupine.js'
-  // });
-  // console.log(obj);
-  // const { loading, error, data } = obj;
-  // if (loading) return 'Loading...';
-  // if (error) return 'An error has occurred';
-
-  // const { module, instance } = data;
-  // return <div>Hello World</div>;
-  const [createfn, setCreateFn] = useState(null);
-  useEffect(() => {
-    const { create } = Porcupine();
-    setCreateFn(create);
-  }, []);
-  return <div />;
+import PicovoiceAudioManager from './picovoiceAudioManager';
+const keywordIDs = {
+  icommand: new Uint8Array([
+    0xcf,
+    0xce,
+    0x91,
+    0x9c,
+    0xcd,
+    0xc4,
+    0x4c,
+    0xcb,
+    0x01,
+    0xb4,
+    0xba,
+    0x71,
+    0xcf,
+    0xff,
+    0xaa,
+    0x66,
+    0xb1,
+    0xf2,
+    0xb3,
+    0xc8,
+    0xd3,
+    0x06,
+    0x3b,
+    0x6e,
+    0x06,
+    0x30,
+    0x60,
+    0x0b,
+    0xcb,
+    0xcb,
+    0x4d,
+    0x03,
+    0xb1,
+    0xae,
+    0x8b,
+    0x6f,
+    0xca,
+    0xd0,
+    0x48,
+    0x0a,
+    0x7f,
+    0xb3,
+    0xe6,
+    0x17,
+    0x43,
+    0x15,
+    0xdb,
+    0xcf,
+    0x5c,
+    0x9e,
+    0xf9,
+    0x31,
+    0x3a,
+    0x26,
+    0xf6,
+    0x83,
+    0x9a,
+    0x48,
+    0x6e,
+    0x54,
+    0x3b,
+    0xaf,
+    0x0c,
+    0xe7,
+    0x29,
+    0x7b,
+    0x0b,
+    0x6a,
+    0xaa,
+    0xf6,
+    0xa9,
+    0xef,
+    0x6f,
+    0x96,
+    0x03,
+    0x3f,
+    0x28,
+    0xb5,
+    0x0f,
+    0xf9,
+    0x67,
+    0x2f,
+    0x22,
+    0xfb,
+    0xd1,
+    0x42,
+    0x9c,
+    0x6c,
+    0xcb,
+    0x88,
+    0xbc,
+    0x36,
+    0x16,
+    0x58,
+    0xe2,
+    0xb9
+  ])
 };
+let keywordNames = Object.keys(keywordIDs);
+let sensitivities = new Float32Array([0.5]);
+
+let currentTimeSeconds = function() {
+  return new Date().getTime() / 1000;
+};
+
+let is_listening = false;
+let listeningStartSeconds;
+let processCallback = function(keywordIndex) {
+  if (keywordIndex === -1) {
+    if (is_listening && currentTimeSeconds() - listeningStartSeconds > 5) {
+      console.log(
+        '(keywordIndex === -1) > && (is_listening && (currentTimeSeconds() - listeningStartSeconds) > 5)'
+      );
+      console.log('turning off listening');
+      is_listening = false;
+    }
+    return;
+  }
+
+  let keyword = keywordNames[keywordIndex];
+  if (is_listening) {
+    if (keyword === 'icommand') {
+      console.log('(is_listening) > && (keyword === "icommand")');
+    }
+    console.log('turning off listening');
+    is_listening = false;
+  }
+};
+let audioManager;
+
+let audioManagerErrorCallback = function(ex) {
+  console.log(ex.toString());
+};
+
+let start = function() {
+  audioManager = new PicovoiceAudioManager();
+  audioManager.start(
+    Porcupine().create(Object.values(keywordIDs), sensitivities),
+    processCallback,
+    audioManagerErrorCallback
+  );
+};
+
+let stop = function() {
+  audioManager.stop();
+};
+
+class Wrapper extends Component {
+  state = {
+    on: false
+  };
+  start = () => {
+    this.setState({ on: true });
+    start();
+  };
+  stop = () => {
+    this.setState({ on: false });
+    stop();
+  };
+  render() {
+    return (
+      <div>
+        <button disabled={this.state.on} onClick={this.start}>
+          Start
+        </button>
+        <button disabled={!this.state.on} onClick={this.stop}>
+          Stop
+        </button>
+      </div>
+    );
+  }
+}
 
 export default Wrapper;
